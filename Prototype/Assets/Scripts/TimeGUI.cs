aee23ebf87732cc7e8a0bfdcbe1b1a88667c2b76
creed30 <creed30@gatech.edu>
2014-09-27 02:21:23 -0400

    Added the time gui

---
+++ Prototype/Assets/Scripts/TimeGUI.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeGUI : MonoBehaviour
+{
+	float milliseconds = 0;
+	int seconds = 0;
+	int minutes = 0;
+	string displayTime;
+
+	void Update()
+	{
+		milliseconds += Time.deltaTime;
+		if (milliseconds > 1)
+		{
+			seconds++;
+			milliseconds = 0;
+		}
+		if (seconds > 60)
+		{
+			minutes++;
+			seconds = 0;
+		}
+	}
+	void OnGUI()
+	{
+		displayTime = minutes.ToString ("00") + ":" + seconds.ToString ("00") + ":" + milliseconds.ToString (".00").Replace (".", "");
+		GUI.Box(new Rect(Screen.width * 0.45f, Screen.height * 0.95f, Screen.width * 0.1f, Screen.height * 0.05f),displayTime);
+	}
+
+}
