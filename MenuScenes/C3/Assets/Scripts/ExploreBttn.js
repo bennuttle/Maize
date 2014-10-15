9fbe85dbd5c54d7e8e3c0a6235c808b0df57c985
R Tyler Lucas <rtylerlucas@gmail.com>
2014-10-15 13:50:09 -0400

    Menu Project
    Named C3

---
+++ MenuScenes/C3/Assets/Scripts/ExploreBttn.js
@@ -0,0 +1,19 @@
+﻿
+//transform.position = Vector3(Screen.height-GameObject.render.bounds.size(), 0, 0);
+//transform.position = Vector3(0, 0, 0);
+function OnMouseUp(){
+
+//load level
+Application.LoadLevel(1);
+
+}
+
+function OnMouseHover(){
+Debug.Log("Hovering Explore");
+}
+
+function Update(){
+//quit game if escape key is pressed
+if (Input.GetKey(KeyCode.Escape)) { Application.Quit();
+}
+}
