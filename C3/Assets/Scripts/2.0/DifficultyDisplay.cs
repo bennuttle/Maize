40c7c8bd66c656d4a869ce7c332f11490dda9cbc
Chad Reed <chadreed@Chads-MacBook-Pro.local>
2015-06-01 19:30:03 -0400

    Added difficulty scene

---
+++ C3/Assets/Scripts/2.0/DifficultyDisplay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DifficultyDisplay : MonoBehaviour {
+	Text text;
+	public Slider difficultySlider;
+	public void updateDifficultyNumber() {
+		text = this.gameObject.GetComponent<Text>();
+		text.text = difficultySlider.value.ToString();
+	}
+}
---
+++ C3/Assets/Scripts/2.0/MainMenuHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuHandler : MonoBehaviour {
+
+	public void SelectButton(int button) {
+		Application.LoadLevel (button);
+	}
+}
