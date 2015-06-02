d045ba018adca5b898f22fdd4e9da06657240570
Chad Reed <chadreed@Chads-MacBook-Pro.local>
2015-06-02 00:09:56 -0400

    Added new scene for maze generation

--- C3/Assets/Scripts/2.0/MainMenuHandler.cs
+++ C3/Assets/Scripts/2.0/MainMenuHandler.cs
@@ -3,7 +3,25 @@ using System.Collections;
 
 public class MainMenuHandler : MonoBehaviour {
 
-	public void SelectButton(int button) {
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Debug.Log(Application.loadedLevel);
+			if (Application.loadedLevel == 0) {
+				Application.Quit();
+			} else if (Application.loadedLevel != 2) {
+				selectButton (0);
+			} else if (Application.loadedLevel != 3) {
+				selectButton (0);
+			} else {
+				selectButton (0);
+			}
+		}
+	}
+
+	public void selectButton(int button) {
 		Application.LoadLevel (button);
 	}
+
+	public void getLevel() {
+		}
 }
